72d477e4e73d8b3b90059463220505bc9aeba340
Akhil Vaidya <akhilvaidya2004@gmail.com>
2024-12-23 22:55:52 +0530

    debug-1

--- modules/abstract_analyzer.py
+++ modules/abstract_analyzer.py
@@ -569,7 +569,7 @@ class AbstractAnalyzer:
                 do_sample=True,
                 pad_token_id=tokenizer.eos_token_id
             )
-            
+            print(tokenizer.decode(outputs[0], skip_special_tokens=True).strip())
             return tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
             
         except Exception as e:
